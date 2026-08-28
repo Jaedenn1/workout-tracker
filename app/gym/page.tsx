b1650ebbfaf5c4da3dd7fdeab86d40e217151729
Jaedenn1 <Jaedennm@gmail.com>
2026-08-27 21:02:36 -0400

    Wire cancel workout session control

--- app/gym/page.tsx
+++ app/gym/page.tsx
@@ -1,5 +1,11 @@
 import FirstWorkoutGymLogger from "../../src/components/FirstWorkoutGymLogger";
+import CancelWorkoutControl from "../../src/components/CancelWorkoutControl";
 
 export default function GymPage() {
-  return <FirstWorkoutGymLogger />;
+  return (
+    <>
+      <FirstWorkoutGymLogger />
+      <CancelWorkoutControl />
+    </>
+  );
 }
